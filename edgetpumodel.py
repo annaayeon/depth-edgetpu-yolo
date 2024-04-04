d70cb2ca8da6f4e7a66067f83afca0e5394257e9
annaayeon <ayeon52@kookmin.ac.kr>
2024-04-04 19:09:48 +0900

    init

--- edgetpumodel.py
+++ edgetpumodel.py
@@ -3,6 +3,7 @@ import os
 import logging
 import rospy
 import math
+
 import yaml
 import numpy as np
 import pycoral.utils.edgetpu as etpu
@@ -10,19 +11,34 @@ from pycoral.adapters import common
 from nms import non_max_suppression
 import cv2
 import json
-import pyrealsense2 as rs
 
 from utils import plot_one_box, Colors, get_image_tensor
 from geometry_msgs.msg import Twist
 
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("EdgeTPUModel")
 angle_pub = rospy.Publisher('move_tracking_Angle_pub', Twist, queue_size=1000)
-goal_pub = rospy.Publisher('goal_position_pub', Twist, queue_size=1000)
+goal_pub = rospy.Publisher('goal_position_pub',Twist,queue_size=1000)
 
 class EdgeTPUModel:
-    def __init__(self, model_file, names_file, cam_pipeline, conf_thresh=0.6, iou_thresh=0.45, filter_classes=None, agnostic_nms=False, max_det=1000):
+
+    def __init__(self, model_file, names_file, conf_thresh=0.6, iou_thresh=0.45, filter_classes=None, agnostic_nms=False, max_det=1000):
+        """
+        Creates an object for running a Yolov5 model on an EdgeTPU
+        
+        Inputs:
+          - model_file: path to edgetpu-compiled tflite file
+          - names_file: yaml names file (yolov5 format)
+          - conf_thresh: detection threshold
+          - iou_thresh: NMS threshold
+          - filter_classes: only output certain classes
+          - agnostic_nms: use class-agnostic NMS
+          - max_det: max number of detections
+        """
+    
         model_file = os.path.abspath(model_file)
+    
         if not model_file.endswith('tflite'):
             model_file += ".tflite"
             
@@ -32,6 +48,7 @@ class EdgeTPUModel:
         self.filter_classes = filter_classes
         self.agnostic_nms = agnostic_nms
         self.max_det = max_det
+
 
         self.no_ball_cnt = 0
         self.m_Pan_err = 0
@@ -50,18 +67,12 @@ class EdgeTPUModel:
         self.inference_time = None
         self.nms_time = None
         self.interpreter = None
-        self.colors = Colors()
+        self.colors = Colors()  # create instance for 'from utils.plots import colors'
         
         self.get_names(names_file)
         self.make_interpreter()
-        self.get_image_size()        
-
-        # RealSense 파이프라인 설정
-        self.pipeline = cam_pipeline
+        self.get_image_size()
         
-    def __del__(self):
-        self.pipeline.stop()
-
     def get_names(self, path):
         """
         Load a names file
@@ -297,80 +308,73 @@ class EdgeTPUModel:
         goal_pub.publish(twist)
 
     def process_predictions(self, det, output_image, pad, output_path="detection.jpg", save_img=True, save_txt=True, hide_labels=False, hide_conf=False):
+        """
+        Process predictions and optionally output an image with annotations
+        """
         xyxy = []
         angle = [0,0]
-        twist = Twist()
+        twist = Twist() 
         ball_flag = 0
-
-        # RealSense 프레임 얻기
-        frames = self.pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
-        if not depth_frame or not color_frame:
-            return
-
-        # Numpy 배열로 변환
-        color_image = np.asanyarray(color_frame.get_data())
-        depth_image = np.asanyarray(depth_frame.get_data())
-
+        
         if len(det):
-            det[:, :4] = self.get_scaled_coords(det[:, :4], output_image, pad)
 
-            ball_det = [det[i, :] for i in range(len(det)) if det[i, 5] == 0 and det[i, 4] >= 0.8]
-            goal_det = [det[i, :] for i in range(len(det)) if det[i, 5] == 1]
-            foot_det = [det[i, :] for i in range(len(det)) if det[i, 5] == 2]
+            # Rescale boxes from img_size to im0 size
+            # x1, y1, x2, y2=
+            det[:, :4] = self.get_scaled_coords(det[:,:4], output_image, pad)
+            '''
+            det[:,:4] 양 옆 좌표  0:x1, 1:y1, 2:x2, 3:y2
+            det[:,4] conf
+            det[:,5] class 0:ball 1:goal 2:foot
+            '''
+
+            ball_det = [det[i,:] for i in range(len(det)) if det[i, 5] == 0 and det[i, 4] >= 0.8]
+            goal_det = [det[i,:] for i in range(len(det)) if det[i, 5] == 1]
+            foot_det = [det[i,:] for i in range(len(det)) if det[i, 5] == 2]
 
             output = {}
             base, ext = os.path.splitext(output_path)
 
             if len(ball_det):
-                angle, ball_xy = self.move_tracking(ball_det)
-                # 공 중심 좌표로 깊이 측정
-                distance = depth_frame.get_distance(int(ball_xy[0]), int(ball_xy[1]))
-                ball_distance = distance * 1000  # 미터를 밀리미터로 변환
+                angle, ball_xy  = self.move_tracking(ball_det)
+                ball_distance = 55 * math.atan(angle[1]) #robot height
                 twist.linear.x = ball_distance
-                ball_flag = 1
+                ball_flag = 1  # yes_ball
                 if len(foot_det) and (ball_xy[1] > 300):
-                    ball_flag = 2
+                    ball_flag = 2   # foot and ball
+                    print('**********flag = 2************')
 
             if len(goal_det):
                 self.goal_position_pub(goal_det)
 
             s = ""
             for c in np.unique(det[:, -1]):
-                n = (det[:, -1] == c).sum()
-                s += f"{n} {self.names[int(c)]}{'s' * (n > 1)}, "
+                n = (det[:, -1] == c).sum()  # detections per class
+                s += f"{n} {self.names[int(c)]}{'s' * (n > 1)}, "  # add to string
             if s != "":
                 s = s.strip()
                 s = s[:-1]
-            logger.info(f"Detected: {s}")
-
+            logger.info("Detected: {}".format(s))
+            # Write results
             for *xyxy, conf, cls in reversed(det):
-                if save_img:
-                    c = int(cls)
+                if save_img:  # Add bbox to image
+                    prev_time = time.time()
+                    c = int(cls)  # integer class
                     label = None if hide_labels else (self.names[c] if hide_conf else f'{self.names[c]} {conf:.2f}')
                     output_image = plot_one_box(xyxy, output_image, label=label, color=self.colors(c, True))
-                    best_ball_det = max(det, key=lambda x: x[4])
-
-                    box_center_x = int((best_ball_det[0] + best_ball_det[2]) / 2)
-                    box_center_y = int((best_ball_det[1] + best_ball_det[3]) / 2)
-                    distance = depth_frame.get_distance(box_center_x, box_center_y)
-
-                   # 깊이 정보를 화면에 표시
-                    cv2.putText(output_image, f"{distance:.2f}m", (box_center_x, box_center_y + 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,0,0), 2)
-                    print('({},{}) distance = [{}m]'.format(box_center_x, box_center_y,distance))
-
                 if save_txt:
-                    xyxy.append(conf)
+                    if xyxy[0]<640 and xyxy[1]<480:
+                        xyxy.append(conf)
+                        if self.names[c]=="ball":
+                            xyxy.append(1)        
                     output[base] = {}
                     output[base]['box'] = xyxy
                     output[base]['conf'] = conf
                     output[base]['cls'] = cls
                     output[base]['cls_name'] = self.names[c]
             if save_txt:
-                output_txt = base + ".txt"
+                output_txt = base+"txt"
                 with open(output_txt, 'w') as f:
-                    json.dump(output, f, indent=1)
+                   json.dump(output, f, indent=1)
             if save_img:
                 cv2.imwrite(output_path, output_image)
 
@@ -380,6 +384,5 @@ class EdgeTPUModel:
         angle_pub.publish(twist)
         cv2.imshow('Camera', output_image)
         cv2.waitKey(1)
-
 
         return det,output_image, xyxy
